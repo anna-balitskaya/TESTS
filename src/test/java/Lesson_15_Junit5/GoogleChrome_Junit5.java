d8f7aaa91ed5f1c5fe3e1d756e0211c70801f884
balit <balitskaya.anna1@gmail.com>
2025-03-19 13:17:49 +0100

    first comit

--- src/test/java/Lesson_15_Junit5/GoogleChrome_Junit5.java
+++ src/test/java/Lesson_15_Junit5/GoogleChrome_Junit5.java
@@ -2,18 +2,17 @@ package Lesson_15_Junit5;
 
 /*
 Test сase
-0. Открыть браузур (Chrome) и перейти поссылке URL https://www.mts.by/
+Открыть браузур (Chrome) и перейти поссылке URL https://www.mts.by/
 и добавить флюент ожидание для появления кнопки "Принять" на модалке
-1. Кликнуть кнопку "Принять" на модалке "Обработка файлов cookie"
-2. Проверить название для блока "Онлайн пополнение"
-3. Проверить наличие логотипов платежных систем
-4. Проверить работу ссылки "Подробнее о сервисе"
-5-8. Заполнить поля платежной формы для "Услуги связи" и кликнуть по кнопке
+Кликнуть кнопку "Принять" на модалке "Обработка файлов cookie"
+1. Проверить название для блока "Онлайн пополнение"
+2. Проверить наличие логотипов платежных систем
+3. Проверить работу ссылки "Подробнее о сервисе"
+4-7. Заполнить поля платежной формы для "Услуги связи", кликнуть по кнопке и проверить переход на окно оплаты
 Закрыть браузер
  */
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
@@ -26,10 +25,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 import java.util.List;
-import java.util.function.Function;
+import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class GoogleChrome_Junit5 {
@@ -39,64 +37,56 @@ public class GoogleChrome_Junit5 {
     @BeforeAll
     static void setupAll() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
     }
 
-    @AfterAll
-    static void teardown() {
+    @BeforeEach
+    void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://www.mts.by/");
+
+        FluentWait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(60))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(Exception.class);
+
+        try {
+            WebElement acceptButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("cookie-agree")));
+            acceptButton.click();
+        } catch (Exception e) {
+            System.out.println("Кнопка \"Принять куки\" не отобразилась");
+
+        }
+    }
+
+    @AfterEach
+    void teardown() {
         if (driver != null) {
             driver.quit();
         }
     }
 
-    @DisplayName("Перейти по ссылке в Google Chrome на сайт МТС")
-    @Test
-    @Order(0)
-    void goToLink() {
-        driver.get("https://www.mts.by/");
-    }
-
-
-    @DisplayName("Принять cookie")
-    @Test
-    @Order(1)
-    void acceptCookie() {
-        FluentWait<WebDriver>wait = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(10))
-                .pollingEvery(Duration.ofMillis(1))
-                .ignoring(Exception.class);
-
-        WebElement acceptButton = wait.until(new Function<WebDriver, WebElement>() {
-            public WebElement apply(WebDriver driver) {
-                return driver.findElement(By.id("cookie-agree"));
-            }
-        });
-        acceptButton.click();
-    }
-
     @DisplayName("Проверка названия блока \"Онлайн пополнение без комиссии\"")
     @Test
-    @Order(2)
-    void checkBlockName () {
+    void checkBlockName() {
         WebElement payWrapper = driver.findElement(By.className("pay__wrapper"));
         WebElement header = payWrapper.findElement(By.tagName("h2"));
-        assertEquals("Онлайн пополнение\nбез комиссии", header.getText().trim(), "Текст заголовка не совпадает.");
-      //  WebElement blockTitle = driver.findElement(By.xpath("//h2[contains(text(),'Онлайн пополнение')]"));
+        assertEquals("Онлайн пополнение\nбез комиссии", header.getText().trim(), "Текст заголовка не совпадает");
     }
+
 
     @DisplayName("Проверка логотипов платежных систем")
     @Test
-    @Order(3)
-    void checkLogos () {
+    void checkLogos() {
         List<WebElement> logos = driver.findElements(By.cssSelector(".pay__partners img"));
         String[] expectedAltValues = {"Visa", "Verified By Visa", "MasterCard", "MasterCard Secure Code", "Белкард"};
     }
 
     @DisplayName("Проверка ссылки \"Подробнее о сервисе\"")
     @Test
-    @Order(4)
-    void checkLink () {
-        WebElement link = driver.findElement(By.linkText("Подробнее о сервисе"));
+    void checkLink() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement link = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Подробнее о сервисе")));
         String href = link.getAttribute("href");
         String expectedHref = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
         assertEquals(expectedHref, href, "Некорректный URL");
@@ -104,40 +94,65 @@ public class GoogleChrome_Junit5 {
 
     @DisplayName("Заполнение поля \"Номер телефона\"")
     @Test
-    @Order(5)
-    void clickAndEnterPhone () {
-        WebElement phoneField = driver.findElement(By.id("connection-phone"));
+    void clickAndEnterPhone() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement phoneField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("connection-phone")));
         phoneField.click();
         String valueToEnter = "297777777";
         phoneField.sendKeys(valueToEnter);
+        String actualValue = phoneField.getAttribute("value").replaceAll("\\D", "");
+        assertEquals(valueToEnter, actualValue, "Введенное значение не соответствует ожидаемому");
     }
 
     @DisplayName("Заполнение поля \"Сумма\"")
     @Test
-    @Order(6)
-    void clickAndEnterSum () {
-        WebElement sumField = driver.findElement(By.id("connection-sum"));
+    void clickAndEnterSum() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement sumField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("connection-sum")));
+        wait.until(ExpectedConditions.elementToBeClickable(sumField));
         sumField.click();
         String valueToEnter = "15";
         sumField.sendKeys(valueToEnter);
+        wait.until(ExpectedConditions.attributeToBe(sumField, "value", valueToEnter));
+        String actualValue = sumField.getAttribute("value");
+        assertEquals(valueToEnter, actualValue, "Введенное значение не соответствует ожидаемому");
     }
 
     @DisplayName("Заполнение поля \"E-mail для отправки чека\"")
     @Test
-    @Order(7)
-    void clickAndEnterEmail () {
+    void clickAndEnterEmail() {
         WebElement emailField = driver.findElement(By.id("connection-email"));
         emailField.click();
         String valueToEnter = "test123test@gmail.com";
         emailField.sendKeys(valueToEnter);
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.attributeToBe(emailField, "value", valueToEnter));
+        String actualValue = emailField.getAttribute("value");
+        assertEquals(valueToEnter, actualValue, "Введенное зачение не соответствует ожидаемому");
     }
 
     @DisplayName("Клик по кнопке \"Продолжить\"")
     @Test
-    @Order(8)
     void payButtonClick() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        clickAndEnterPhone();
+        clickAndEnterSum();
+        clickAndEnterEmail();
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
         WebElement payButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.button.button__default")));
         payButton.click();
+        System.out.println("Кнопка \"Продолжить\" успешно нажата");
+
+        WebElement iframeElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//iframe[@class='bepaid-iframe'])[1]")));
+        assertNotNull(iframeElement, "Не удалось найти iframe");
+
+        driver.switchTo().frame(iframeElement);
+        System.out.println("Переключение на iframe успешно");
+
+        WebElement paymentForm = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='payment-page__container']")));
+        System.out.println("Окно оплаты отображается");
+        assertNotNull(paymentForm, "Форма для оплаты отсутствует");
+
+        driver.switchTo().defaultContent();
     }
 }
--- src/test/java/Lesson_15_TestNG/GoogleChrome_TestNG.java
+++ src/test/java/Lesson_15_TestNG/GoogleChrome_TestNG.java
@@ -2,13 +2,13 @@ package Lesson_15_TestNG;
 
 /*
 Test сase
-0. Открыть браузур (Chrome) и перейти поссылке URL https://www.mts.by/
+Открыть браузур (Chrome) и перейти поссылке URL https://www.mts.by/
 и добавить флюент ожидание для появления кнопки "Принять" на модалке
-1. Кликнуть кнопку "Принять" на модалке "Обработка файлов cookie"
-2. Проверить название для блока "Онлайн пополнение"
-3. Проверить наличие логотипов платежных систем
-4. Проверить работу ссылки "Подробнее о сервисе"
-5-8. Заполнить поля платежной формы для "Услуги связи" и кликнуть по кнопке
+Кликнуть кнопку "Принять" на модалке "Обработка файлов cookie"
+1. Проверить название для блока "Онлайн пополнение"
+2. Проверить наличие логотипов платежных систем
+3. Проверить работу ссылки "Подробнее о сервисе"
+4-7. Заполнить поля платежной формы для "Услуги связи", кликнуть по кнопке и проверить переход на окно оплаты
 Закрыть браузер
  */
 
@@ -22,16 +22,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
 
 import java.time.Duration;
 import java.util.List;
-import java.util.function.Function;
+import java.util.NoSuchElementException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GoogleChrome_TestNG {
 
@@ -40,86 +40,115 @@ public class GoogleChrome_TestNG {
     @BeforeClass
     static void setupAll() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
     }
 
-    @AfterClass
+    @BeforeMethod
+    public void setup() {
+        driver = new ChromeDriver();
+        driver.get("https://www.mts.by/");
+
+        FluentWait<WebDriver> wait = new FluentWait<>(driver)
+                .withTimeout(Duration.ofSeconds(60))
+                .pollingEvery(Duration.ofMillis(500))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(Exception.class);
+
+        try {
+            WebElement acceptButton = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("cookie-agree")));
+            acceptButton.click();
+        } catch (Exception e) {
+            System.out.println("Кнопка \"Принять куки\" не отобразилась");
+
+        }
+    }
+
+    @AfterMethod
     static void teardown() {
         if (driver != null) {
             driver.quit();
         }
     }
 
-    @Test(priority = 0, description = "Перейти по ссылке в Google Chrome на сайт МТС")
-    void goToLink() {
-        driver.get("https://www.mts.by/");
-    }
-
-    @Test(priority = 1, description = "Принять cookie", dependsOnMethods = {"goToLink"})
-    void acceptCookie() {
-        FluentWait<WebDriver> wait = new FluentWait<>(driver)
-                .withTimeout(Duration.ofSeconds(10))
-                .pollingEvery(Duration.ofMillis(1))
-                .ignoring(Exception.class);
-
-        WebElement acceptButton = wait.until(new Function<WebDriver, WebElement>() {
-            public WebElement apply(WebDriver driver) {
-                return driver.findElement(By.id("cookie-agree"));
-            }
-        });
-        acceptButton.click();
-    }
-
-    @Test(priority = 2, description = "Проверка названия блока \"Онлайн пополнение без комиссии\"", dependsOnMethods = {"acceptCookie"})
+    @Test(description = "Проверка названия блока \"Онлайн пополнение без комиссии\"")
     void checkBlockName() {
-        WebElement payWrapper = driver.findElement(By.className("pay__wrapper"));
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement payWrapper = wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("pay__wrapper")));
         WebElement header = payWrapper.findElement(By.tagName("h2"));
         assertEquals("Онлайн пополнение\nбез комиссии", header.getText().trim(), "Текст заголовка не совпадает.");
-        //  WebElement blockTitle = driver.findElement(By.xpath("//h2[contains(text(),'Онлайн пополнение')]"));
     }
 
-    @Test(priority = 3, description = "Проверка логотипов платежных систем", dependsOnMethods = {"checkBlockName"})
+    @Test(description = "Проверка логотипов платежных систем")
     void checkLogos() {
         List<WebElement> logos = driver.findElements(By.cssSelector(".pay__partners img"));
         String[] expectedAltValues = {"Visa", "Verified By Visa", "MasterCard", "MasterCard Secure Code", "Белкард"};
     }
 
-    @Test(priority = 4, description = "Проверка ссылки \"Подробнее о сервисе\"", dependsOnMethods = {"checkLogos"})
-    void checkLink() {
-        WebElement link = driver.findElement(By.linkText("Подробнее о сервисе"));
-        String href = link.getAttribute("href");
-        String expectedHref = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
-        assertEquals(expectedHref, href, "Некорректный URL");
-    }
+        @Test(description = "Проверка ссылки \"Подробнее о сервисе\"")
+        void checkLink () {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement link = wait.until(ExpectedConditions.visibilityOfElementLocated(By.linkText("Подробнее о сервисе")));
+            String href = link.getAttribute("href");
+            String expectedHref = "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/";
+            assertEquals(expectedHref, href, "Некорректный URL");
+        }
 
-    @Test(priority = 5, description = "Заполнение поля \"Номер телефона\"", dependsOnMethods = {"checkLink"})
-    void clickAndEnterPhone() {
-        WebElement phoneField = driver.findElement(By.id("connection-phone"));
-        phoneField.click();
-        String valueToEnter = "297777777";
-        phoneField.sendKeys(valueToEnter);
-    }
+        @Test(description = "Заполнение поля \"Номер телефона\"")
+        void clickAndEnterPhone () {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement phoneField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("connection-phone")));
+            phoneField.click();
+            String valueToEnter = "297777777";
+            phoneField.sendKeys(valueToEnter);
+            String actualValue = phoneField.getAttribute("value").replaceAll("\\D", "");
+            assertEquals(valueToEnter, actualValue, "Введенное значение не соответствует ожидаемому");
+        }
 
-    @Test(priority = 6, description = "Заполнение поля \"Сумма\"", dependsOnMethods = {"clickAndEnterPhone"})
-    void clickAndEnterSum() {
-        WebElement sumField = driver.findElement(By.id("connection-sum"));
-        sumField.click();
-        String valueToEnter = "15";
-        sumField.sendKeys(valueToEnter);
-    }
+        @Test(description = "Заполнение поля \"Сумма\"")
+        void clickAndEnterSum () {
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            WebElement sumField = wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("connection-sum")));
+            wait.until(ExpectedConditions.elementToBeClickable(sumField));
+            sumField.click();
+            String valueToEnter = "15";
+            sumField.sendKeys(valueToEnter);
+            wait.until(ExpectedConditions.attributeToBe(sumField, "value", valueToEnter));
+            String actualValue = sumField.getAttribute("value");
+            assertEquals(valueToEnter, actualValue, "Введенное значение не соответствует ожидаемому");
+        }
 
-    @Test(priority = 7, description = "Заполнение поля \"E-mail для отправки чека\"", dependsOnMethods = {"clickAndEnterSum"})
-    void clickAndEnterEmail() {
-        WebElement emailField = driver.findElement(By.id("connection-email"));
-        emailField.click();
-        String valueToEnter = "test123test@gmail.com";
-        emailField.sendKeys(valueToEnter);
-    }
+        @Test(description = "Заполнение поля \"E-mail для отправки чека\"")
+        void clickAndEnterEmail () {
+            WebElement emailField = driver.findElement(By.id("connection-email"));
+            emailField.click();
+            String valueToEnter = "test123test@gmail.com";
+            emailField.sendKeys(valueToEnter);
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.attributeToBe(emailField, "value", valueToEnter));
+            String actualValue = emailField.getAttribute("value");
+            assertEquals(valueToEnter, actualValue, "Введенное значение не соответствует ожидаемому");
+        }
 
-    @Test(priority = 8, description = "Клик по кнопке \"Продолжить\"", dependsOnMethods = {"clickAndEnterEmail"})
-    void payButtonClick() {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
-        WebElement payButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.button.button__default")));
-        payButton.click();
-    }
+        @Test(description = "Клик по кнопке \"Продолжить\"")
+        void payButtonClick () {
+            clickAndEnterPhone();
+            clickAndEnterSum();
+            clickAndEnterEmail();
+
+            WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(60));
+            WebElement payButton = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.button.button__default")));
+            payButton.click();
+            System.out.println("Кнопка \"Продолжить\" успешно нажата");
+
+            WebElement iframeElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("(//iframe[@class='bepaid-iframe'])[1]")));
+            assertNotNull(iframeElement, "Не удалось найти iframe");
+
+            driver.switchTo().frame(iframeElement);
+            System.out.println("Переключение на iframe успешно");
+
+            WebElement paymentForm = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='payment-page__container']")));
+            System.out.println("Окно оплаты отображается");
+            assertNotNull(paymentForm, "Форма для оплаты отсутствует");
+
+            driver.switchTo().defaultContent();
+        }
 }
