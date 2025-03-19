1045f7bd0f41cf6f3e512fbfdbef4d980f1d2e6d
balit <balitskaya.anna1@gmail.com>
2025-03-19 13:35:27 +0100

    first comit

--- src/test/java/Lesson_15_2_Junit5/GoogleChrome_Junit5.java
+++ src/test/java/Lesson_15_2_Junit5/GoogleChrome_Junit5.java
@@ -14,6 +14,7 @@ Test сase
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.*;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -21,6 +22,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 import java.util.List;
 import java.util.NoSuchElementException;
--- src/test/java/Lesson_15_2_TestNG/GoogleChrome_TestNG.java
+++ src/test/java/Lesson_15_2_TestNG/GoogleChrome_TestNG.java
@@ -20,10 +20,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
 import java.time.Duration;
 import java.util.List;
 import java.util.NoSuchElementException;
